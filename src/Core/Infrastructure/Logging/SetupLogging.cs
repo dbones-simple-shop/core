8b2ba064c3fddbb489f7dddc098a98ee7fd6977d
david rundle <dbones>
2019-09-09 03:24:23 +0100

    added the service name

--- src/Core/Infrastructure/Logging/SetupLogging.cs
+++ src/Core/Infrastructure/Logging/SetupLogging.cs
@@ -5,6 +5,8 @@ using Serilog.Formatting.Elasticsearch;
 
 namespace Core.Infrastructure.Logging
 {
+    using System.Reflection;
+
     public static class SetupLogging 
     {
         public static IWebHostBuilder ConfigureLogging(this IWebHostBuilder builder)
@@ -24,7 +26,8 @@ namespace Core.Infrastructure.Logging
                 }
                 else if (loggerConfig.Logger == "datadog")
                 {
-                    config.WriteTo.DatadogLogs(loggerConfig.DataDogApiKey);
+                    string serviceName = Assembly.GetEntryAssembly().GetName().Name;
+                    config.WriteTo.DatadogLogs(loggerConfig.DataDogApiKey, service: serviceName);
                 }
 
                 else
